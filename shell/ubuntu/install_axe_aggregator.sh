d07c472c50e4e454d17253ab33342c6620ef2776
Daniel Seybold <daniel.seybold@uni-ulm.de>
2016-01-19 16:34:27 +0100

    improved start command for axe aggregator

--- shell/ubuntu/install_axe_aggregator.sh
+++ shell/ubuntu/install_axe_aggregator.sh
@@ -15,4 +15,4 @@ git clone https://github.com/cloudiator/axe-aggregator.git
 mvn clean install -f axe-aggregator/pom.xml
 
 #starting the axe-aggregator, default config requires KairosDB at localhost:8080
-nohup java -jar axe-aggregator/service/target/axe-aggregator-service-0.2.0-SNAPSHOT-jar-with-dependencies.jar > axe-aggregator.out & 
+nohup java -jar service/target/axe-aggregator-service-0.2.0-SNAPSHOT-jar-with-dependencies.jar > /dev/null 2>&1 & 
