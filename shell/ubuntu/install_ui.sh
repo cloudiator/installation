3649f8e40001dc2bf86da1b25ecf681de9f4b613
Daniel Baur <daniel.baur@uni-ulm.de>
2016-06-30 13:35:05 +0200

    added apache2 config

--- shell/ubuntu/install_ui.sh
+++ shell/ubuntu/install_ui.sh
@@ -8,11 +8,18 @@ fi
 
 apt-get install apache2 php5 php5-curl
 
-a2enmod rewrite && service apache2 restart
+a2enmod rewrite
 
 git clone https://tuleap.ow2.org/plugins/git/paasage/executionware_ui.git
+
+rm -f /etc/apache2/sites-enabled/*
+cp ui.conf /etc/apache/sites-enabled/
 
 rm -rf /var/www/html/executionware_ui
 mv executionware_ui /var/www/html/
 
 chown www-data:www-data /var/www/html/executionware_ui
+
+service apache2 restart
+
+
