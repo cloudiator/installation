02d224765c742e30b3ed860c3432cde41c98f26a
Daniel Seybold <daniel.seybold@uni-ulm.de>
2016-09-29 17:14:56 +0200

    changed kairos install to upstart

--- shell/ubuntu/install_kairosdb.sh
+++ shell/ubuntu/install_kairosdb.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+echo 'creating folder kairosdb'
+mkdir -p ~/cloudiator
+
+cd cloudiator
+
 #download kairosdb
+echo 'downloading kairosdb'
 wget https://github.com/kairosdb/kairosdb/releases/download/v1.1.1/kairosdb-1.1.1-1.tar.gz -O kairosdb.tar.gz
 
+wget https://raw.githubusercontent.com/cloudiator/installation/master/shell/ubuntu/kairosdb.conf -O /etc/init/kairosdb.conf
+
 #extract kairosdb
+echo 'extracting kairosdb'
 tar -xzf kairosdb.tar.gz
 
 #start kairosdb (with default datastore H2) in background
-kairosdb/bin/kairosdb.sh start
+#kairosdb/bin/kairosdb.sh start
+
+start kairosdb
