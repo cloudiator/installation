017e7c2a671d2c84808b39db5bab9d8837cf234c
Daniel Seybold <daniel.seybold@uni-ulm.de>
2016-09-30 09:51:53 +0200

    changed colosseum installation to binary

--- shell/ubuntu/install_colosseum.sh
+++ shell/ubuntu/install_colosseum.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VERSION=0.2.0
+
 # Read Password
 echo -n "Password for mysql database:"
 read -s MYSQLPW
@@ -17,18 +19,38 @@ local_address=$(dig +short myip.opendns.com @resolver1.opendns.com)
 SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 #download the colosseum master repo 
-git clone https://github.com/cloudiator/colosseum.git
+#git clone https://github.com/cloudiator/colosseum.git
+
+echo 'creating folder axe-aggregator'
+mkdir -p ~/cloudiator
+
+cd ~/cloudiator
+echo 'downloading latest binary, version ${VERSION}'
+wget https://omi-dev.e-technik.uni-ulm.de/jenkins/job/cloudiator-axe-aggregator/lastSuccessfulBuild/artifact/service/target/axe-aggregator-service-0.2.0-SNAPSHOT-jar-with-dependencies.jar
+
+unzip /colosseum-$VERSION-SNAPSHOT.zip
+
+cd colosseum-$VERSION-SNAPSHOT
+
+
+
+#download default config
+echo 'creating colosseum config'
+mkdir conf
+wget https://raw.githubusercontent.com/cloudiator/colosseum/master/conf/application.conf
 
 #create config file
-touch colosseum/conf/config.conf
+touch config.conf
 
-echo "include \"application.conf\"" >>  colosseum/conf/config.conf
-echo "play.crypto.secret =\"$SECRET\"" >>  colosseum/conf/config.conf
-echo "db.default.driver=org.mariadb.jdbc.Driver" >>  colosseum/conf/config.conf 
-echo "db.default.url=\"mysql://root:$MYSQLPW@localhost/colosseum\"" >>  colosseum/conf/config.conf
-echo "colosseum.nodegroup = \"$NODEGROUP\"" >> colosseum/conf/config.conf
+echo "include \"application.conf\"" >>  config.conf
+echo "play.crypto.secret =\"$SECRET\"" >>  config.conf
+echo "db.default.driver=org.mariadb.jdbc.Driver" >>  config.conf 
+echo "db.default.url=\"mysql://root:$MYSQLPW@localhost/colosseum\"" >>  config.conf
+echo "colosseum.nodegroup = \"$NODEGROUP\"" >> config.conf
 
 #start colosseum in a screen, requires etcd running on localhost, otherwise change the respective args
-cd colosseum
-screen -S colosseum -d -m ../activator-dist-1.3.6/activator -Dconfig.file=conf/config.conf -Dlca.client.config.registry=etcdregistry -Dlca.client.config.registry.etcd.hosts=${local_address} run
-cd ..
+cd ../bin
+./colosseum -Dconfig.file=../conf/config.conf
+#cd colosseum
+#screen -S colosseum -d -m ../activator-dist-1.3.6/activator -Dconfig.file=conf/config.conf -Dlca.client.config.registry=etcdregistry -Dlca.client.config.registry.etcd.hosts=${local_address} run
+#cd ..
