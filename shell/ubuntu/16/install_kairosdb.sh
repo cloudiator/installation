5ec0bbb485dde631a00b5b2c98b89f1e096a452e
Daniel Seybold <daniel.seybold@uni-ulm.de>
2017-11-16 13:51:11 +0100

    added systemd files

--- shell/ubuntu/16/install_kairosdb.sh
+++ shell/ubuntu/16/install_kairosdb.sh
@@ -9,7 +9,7 @@ cd /opt/cloudiator
 echo 'downloading kairosdb'
 wget https://github.com/kairosdb/kairosdb/releases/download/v1.1.1/kairosdb-1.1.1-1.tar.gz -O kairosdb.tar.gz
 
-wget https://raw.githubusercontent.com/cloudiator/installation/master/shell/ubuntu/kairosdb.conf -O /etc/systemd/system/kairosdb.service
+wget https://raw.githubusercontent.com/cloudiator/installation/master/shell/ubuntu/16/kairosdb.service -O /etc/systemd/system/kairosdb.service
 
 #extract kairosdb
 echo 'extracting kairosdb'
--- shell/ubuntu/README.md
+++ shell/ubuntu/README.md
@@ -2,7 +2,7 @@
 System service: upstart
 
 
-#Ubuntu 16.04
+# Ubuntu 16.04
 
 System service: systemd
 
