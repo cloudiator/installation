157ebc4a71b0eb61b419d198c509123490f7453c
Daniel Baur <dbaur@users.noreply.github.com>
2017-11-20 14:06:23 +0100

    Update install_axe_aggregator.sh

--- shell/ubuntu/14/install_axe_aggregator.sh
+++ shell/ubuntu/14/install_axe_aggregator.sh
@@ -20,7 +20,7 @@ echo 'downloading latest binary'
 wget  https://oss.sonatype.org/content/repositories/snapshots/io/github/cloudiator/axe/axe-aggregator-service/0.2.0-SNAPSHOT/axe-aggregator-service-0.2.0-20161026.111801-20-jar-with-dependencies.jar -O axe-aggregator-service-0.2.0-SNAPSHOT-jar-with-dependencies.jar
 
 echo 'downloading upstart config'
-wget https://raw.githubusercontent.com/cloudiator/installation/master/shell/ubuntu/axe-aggregator.conf -O /etc/init/axe-aggregator.conf
+wget https://raw.githubusercontent.com/cloudiator/installation/master/shell/ubuntu/14/axe-aggregator.conf -O /etc/init/axe-aggregator.conf
 
 echo 'starting axe-aggregator'
 start axe-aggregator
